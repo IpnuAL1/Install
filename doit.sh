56c32c35dc4b37d164975af6e42a6b999542fd90
IpnuAL1 <74335071+IpnuAL1@users.noreply.github.com>
2020-11-12 09:33:30 +0700

    Update and rename doit.sh to run

--- doit.sh
+++
@@ -1,2 +0,0 @@
-!sudo apt-get install build-essential automake libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev zlib1g-dev git
-!wget https://github.com/JayDDee/cpuminer-opt/archive/v3.15.1.zip
